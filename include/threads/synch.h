06faa8d57f3367a89bd25121bf03d46f4ee3f01c
Doyoung619 <dyheo619@kaist.ac.kr>
2024-10-06 21:12:21 +0900

    Update synch.h

--- include/threads/synch.h
+++ include/threads/synch.h
@@ -21,7 +21,7 @@ struct lock {
 	struct thread *holder;      /* Thread holding lock (for debugging). */
 	struct semaphore semaphore; /* Binary semaphore controlling access. */
 };
-
+bool sema_compare_priority (const struct list_elem *l, const struct list_elem *s, void *aux UNUSED);
 void lock_init (struct lock *);
 void lock_acquire (struct lock *);
 bool lock_try_acquire (struct lock *);
